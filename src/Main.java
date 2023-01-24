1072c65811134108cd922927aa76699951773807
Silvyu <mirceasilvyu@gmail.com>
2023-01-24 10:41:16 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main{
+
+    public static void main(String[] args) {
+
+        double x;
+        double y;
+        double z;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Side X:");
+        x = scanner.nextDouble();
+        System.out.println("Enter Side Y:");
+        y = scanner.nextDouble();
+        z = Math.sqrt((x*x)+(y*y));
+        System.out.println("The hypotenuse is: "+z);
+        scanner.close();
+
+    }
+}
